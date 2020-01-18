81915a736d8b72190f844f1fcdc8a7f4e2dc67b3
jsonta <56021506+jsonta@users.noreply.github.com>
2020-01-18 21:25:35 +0100

    Revert "Zainstalowano Swagger UI oraz zaktualizowano projekt do struktury .NET Core 3.1"
    This reverts commit 5f6131461f828a5f71b44ce08718d54167a1c0a3.

--- Hotel/Startup.cs
+++ Hotel/Startup.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Hotel.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Hotel
 {
@@ -24,11 +30,6 @@ namespace Hotel
             services.AddDbContext<HotelContext>(opt =>
             opt.UseInMemoryDatabase("Hotel"));
             services.AddControllers();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,19 +40,11 @@ namespace Hotel
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
-                c.RoutePrefix = string.Empty;
-            });
-
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
